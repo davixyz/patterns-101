f57c800dda26c87a555b1bf651627061caea408d
Carlos Castro <cdcastrofelix@gmail.com>
2023-07-20 11:38:31 -0700

    add playground exercises for optional chaining and nullish coalescing

--- playground/nullish-coalescing.js
+++ playground/nullish-coalescing.js
@@ -1,1 +1,31 @@
 // Playground for Nullish coalescing operator
+const foo = null ?? "default string";
+console.log(foo);
+
+const foo2 = undefined ?? "default string";
+console.log(foo2);
+
+const obj = {
+  a: 0,
+};
+
+// Old way verbose to access a property
+
+const property =
+  obj.a !== undefined || obj.a !== null ? obj.a : "default value";
+
+console.log("old way", property);
+
+// Nullish coalescing way
+
+const property2 = obj.a ?? "default value";
+
+console.log("new way", property2);
+
+const property3 = obj.b?.c?.d ?? "default value";
+
+const something = "";
+
+if (something) {
+  console.log("something is truthy");
+}
--- playground/optional-chaining.js
+++ playground/optional-chaining.js
@@ -1,4 +1,5 @@
 // Practice Optional Chaining
+const lodash = require("lodash");
 
 const deeplyNestedObject = {
   a: {
@@ -9,8 +10,35 @@ const deeplyNestedObject = {
         },
       },
     },
+    carlos: {
+      another: "another",
+    },
+    truthy: 0,
   },
 };
+
+// First Legacy way of accessing deeply nested props
+const another =
+  deeplyNestedObject &&
+  deeplyNestedObject.a &&
+  deeplyNestedObject.a.carlos &&
+  deeplyNestedObject.a.carlos.another;
+
+console.log("and and", another);
+
+// Second way of accessing deeply nested props
+const another2 = lodash.get(
+  deeplyNestedObject,
+  "a.carlos.another",
+  "default value"
+);
+
+console.log("lodash", another2);
+
+// Third way of accessing deeply nested props
+const another3 = deeplyNestedObject?.a?.truthy;
+
+console.log("optional chaining", another3);
 
 const objectWithFn = {
   a: function () {
@@ -18,4 +46,8 @@ const objectWithFn = {
   },
 };
 
+objectWithFn.a?.();
+
 const randomArr = [1, 2, 3, 4, 5];
+
+console.log(randomArr?.[13212431241]);
